4bc56eadf6e6bf15a16e8dc164e7c24af383e8b8
shieldgenerator7 <shieldgenerator7@yahoo.com>
2019-07-08 17:19:56 -0400

    Renamed Form1

--- BoardGamePaint/Form1.cs
+++ BoardGamePaint/Form1.cs
@@ -9,11 +9,16 @@ using System.Windows.Forms;
 
 namespace BoardGamePaint
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
