7e2c6bf6f86fe7bad9bfbcc659d3a9b26910f834
shieldgenerator7 <shieldgenerator7@yahoo.com>
2019-08-07 15:14:16 -0400

    Added unit test project

---
+++ BoardGamePaintTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BoardGamePaint;
+
+namespace BoardGamePaintTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
