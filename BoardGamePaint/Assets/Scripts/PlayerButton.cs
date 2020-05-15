7548d73361c94a1e54be86f85547ad485691e39c
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-05-14 22:45:48 -0400

    Extract UI references from PlayerButton

--- BoardGamePaint/Assets/Scripts/PlayerButton.cs
+++ BoardGamePaint/Assets/Scripts/PlayerButton.cs
@@ -1,24 +1,12 @@
 ﻿using System;
-using System.Drawing;
-using System.Drawing.Imaging;
 
 public class PlayerButton : Button
 {
     public Player player { get; private set; }
 
-    private ImageAttributes attr;
-
-    public PlayerButton(Player player, int size) : base(ImageUtility.getImage("player"), size)
+    public PlayerButton(Player player) : base(null)
     {
         this.player = player;
-        //2019-09-09: ColorMap code copied from https://stackoverflow.com/a/27101587/2336212
-        // Set the image attribute's color mappings
-        ColorMap[] colorMap = new ColorMap[1];
-        colorMap[0] = new ColorMap();
-        colorMap[0].OldColor = Color.White;
-        colorMap[0].NewColor = (this.player) ? this.player.color : Color.Gray;
-        attr = new ImageAttributes();
-        attr.SetRemapTable(colorMap);
     }
 
     public override void activate()
@@ -29,26 +17,5 @@ public class PlayerButton : Button
     public override string TypeString
     {
         get => (this.player) ? this.player.name : "Neutral Player";
-    }
-
-    public override void draw(Graphics graphics)
-    {
-        // Draw using the color map
-        Rectangle rect = new Rectangle(
-            (int)TopLeftScreen.x,
-            (int)TopLeftScreen.y,
-            size.Width,
-            size.Height);
-        graphics.DrawImage(
-            image,
-            rect,
-            0,
-            0,
-            image.Size.Width,
-            image.Size.Height,
-            GraphicsUnit.Pixel,
-            attr
-            );
-        drawFooterNumber(graphics);
     }
 }
---
+++ BoardGamePaint/Assets/Scripts/PlayerButtonSprite.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+public class PlayerButtonSprite:ButtonSprite
+{
+    private PlayerButton playerButton { get => (PlayerButton)gameObject; }
+
+	private Image playerImage;
+	public override Image image { 
+		get => playerImage;
+		protected set => throw new NotImplementedException();
+	}
+
+	private ImageAttributes attr;
+
+	public PlayerButtonSprite(PlayerButton playerButton, int size):base(playerButton,size)
+	{
+		playerImage = ImageUtility.getImage("player");
+		//2019-09-09: ColorMap code copied from https://stackoverflow.com/a/27101587/2336212
+		// Set the image attribute's color mappings
+		ColorMap[] colorMap = new ColorMap[1];
+		colorMap[0] = new ColorMap();
+		colorMap[0].OldColor = Color.White;
+		colorMap[0].NewColor = (this.playerButton.player) ? this.playerButton.player.color : Color.Gray;
+		attr = new ImageAttributes();
+		attr.SetRemapTable(colorMap);
+    }
+
+    public override void draw(Graphics graphics)
+    {
+        // Draw using the color map
+        Rectangle rect = new Rectangle(
+            (int)TopLeftScreen.x,
+            (int)TopLeftScreen.y,
+            size.Width,
+            size.Height);
+        graphics.DrawImage(
+            image,
+            rect,
+            0,
+            0,
+            image.Size.Width,
+            image.Size.Height,
+            GraphicsUnit.Pixel,
+            attr
+            );
+        drawFooterNumber(graphics);
+    }
+}
