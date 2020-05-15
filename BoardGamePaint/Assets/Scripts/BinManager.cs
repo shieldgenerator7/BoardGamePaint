c951ee8bd26e66c6c6a413dd3f9c09a6495057d1
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-05-14 23:26:34 -0400

    Extract UI references from BinManager

--- BoardGamePaint/Assets/Scripts/BinManager.cs
+++ BoardGamePaint/Assets/Scripts/BinManager.cs
@@ -1,7 +1,5 @@
-﻿using BoardGamePaint;
+﻿
 using System;
-using System.Collections.Generic;
-using System.Drawing;
 
 public class BinManager : Tray
 {
@@ -11,12 +9,7 @@ public class BinManager : Tray
 
     public void makeBin(GameObject gameObject)
     {
-        Bin newBin = new Bin(gameObject, componentSize);
+        Bin newBin = new Bin(gameObject);
         addComponent(newBin);
-    }
-
-    public Bin getBin(Vector mousePos)
-    {
-        return (Bin)getComponent(mousePos);
     }
 }
---
+++ BoardGamePaint/Assets/Scripts/BinManagerSprite.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class BinManagerSprite:TraySprite
+{
+	public BinManagerSprite(BinManager binManager):base(binManager)
+	{
+    }
+
+    public Bin getBin(Vector mousePos)
+    {
+        return (Bin)getComponent(mousePos);
+    }
+}
